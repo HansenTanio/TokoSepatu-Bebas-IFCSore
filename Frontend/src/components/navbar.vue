e6f9d723fba0fc74fafdcf0720ca7545020be651
Hansen Tanio <hansentanio33@gmail.com>
2023-05-20 13:09:18 +0700

    membuat navbar

---
+++ Frontend/src/components/navbar.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-lg" style="background-color: darkorange;">
+            <img src="../../../public/icons/Logo.png" alt="Logo" style="width: 50px; margin-right: 10px;">
+            <h4 class="navbar-brand text-light" href="#" >Bebas Shop</h4>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto"></ul>
+                <a class="btn btn-outline-light my-2 my-sm-0 mx-2" href="/">Login/Logout</a>
+                <a class="btn btn-outline-light my-2 my-sm-0 mx-2" href="/signUp">Sign Up</a>
+            </div>
+        </nav>
+    </div>
+</template>
