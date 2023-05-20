e77f6b64b1b5dcb5986f27c1a8b54762990d6943
Hansen Tanio <hansentanio33@gmail.com>
2023-05-20 14:27:54 +0700

    membuat sign up page dan login page

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,8 +1,12 @@
 <script>
 import Navbar from './components/Navbar.vue'
+import Login from './components/login.vue'
+import signUp from './components/signup.vue'
 export default {
   components: {
     Navbar,
+    Login,
+    signUp
   }
 }
 </script>
@@ -10,5 +14,7 @@ export default {
 <template>
   <div>
     <Navbar></Navbar>
+    <Login></Login>
+    <signUp></signUp>
   </div>
 </template>
---
+++ Frontend/src/components/login.vue
@@ -0,0 +1,21 @@
+<template>
+    <div style="display: center;">
+        <div class="container col-sm-4 bg-light" style="height: 50%; margin: auto;margin-top: 120px;width: 50%;border: 3px solid darkorange;padding: 20px;border-radius: 30px;">
+            <div class="mx-auto col-sm-4" style="font-size: 30px; font-weight: bold;">
+                Login
+            </div>
+            <form>
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Username</label>
+                    <input type="text" class="form-control" id="username" placeholder="Username">
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input type="password" class="form-control" id="password" placeholder="Password">
+                </div>
+                <a class="btn text-white" style="background-color: darkorange;">Submit</a>
+            </form>
+            <p>Belum punya akun? <a href="/signUp">Daftar Sekarang</a></p>
+        </div>
+    </div>
+</template>
